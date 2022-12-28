030fd9477b4cbfc86e3898e19a7799be760b7cbc
wangzw <wzw571603974@qq.com>
2022-12-28 18:29:35 +0800

    feat：加跨域

--- middleware/user_middleware.go
+++ middleware/user_middleware.go
@@ -2,8 +2,10 @@ package middleware
 
 import (
 	"errors"
+	"fmt"
 	"gin_storage/service"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // 返回当前登录用户
@@ -15,4 +17,24 @@ func CurrentUser(context *gin.Context) (u interface{}, err error) {
 	}
 	user, err := service.ServiceGroupApp.GetUserById(userId)
 	return user, nil
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		fmt.Println(method)
+		fmt.Println(origin)
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		c.Next()
+	}
 }
--- router/user_router.go
+++ router/user_router.go
@@ -9,14 +9,13 @@ import (
 type LoginApi struct{}
 
 func (l *LoginApi) LoginRouters(Router *gin.RouterGroup) {
-	r := Router.Group("user") //.Use(middleware.TokenMiddleware())
+	r := Router.Group("user").Use(middleware.Cors()) //.Use(middleware.TokenMiddleware())
 	{
 		r.POST("/login", api.ApiGroupApp.Login)                                                  // 登录接口
 		r.POST("/register", api.ApiGroupApp.Register)                                            //注册
 		r.GET("/getCaptcha", api.ApiGroupApp.GetCaptcha)                                         // 获取验证码
 		r.GET("/getUserInfo", middleware.JWTAuthMiddleware(), api.ApiGroupApp.GetUserInfo)       // 查看当前用户信息
 		r.PUT("/updateUserInfo/:id", middleware.JWTAuthMiddleware(), api.ApiGroupApp.UpdateUser) // 修改用户信息
-		r.GET("/index", api.ApiGroupApp.TestHtml)
 	}
 }
 
